1827f78c1e662b546e7bb349607bc6813fc5b27b
noambeckmann <noambeckmann@gmail.com>
2014-03-10 14:42:00 -0400

    save_intermediary_files

--- src/unstacking.R
+++ src/unstacking.R
@@ -1,4 +1,4 @@
-unstacking = function(pwd,pwd_bacteria_reads_nat,pwd_bacteria_reads_wga,fasta){
+unstacking = function(pwd,pwd_bacteria_reads_nat,pwd_bacteria_reads_wga,fasta,save_intermediary_files){
 #################
 # This function is unstacking the IPD file
 # pwd is the location in which we want to save our output
@@ -8,13 +8,16 @@ unstacking = function(pwd,pwd_bacteria_reads_nat,pwd_bacteria_reads_wga,fasta){
 #################
 
 	setwd(pwd)
-	load(pwd_bacteria_reads_nat)
-	load(pwd_bacteria_reads_wga)
-
+	if (save_intermediary_files){
+		load(pwd_bacteria_reads_nat)
+		load(pwd_bacteria_reads_wga)
+	}
+	
 	ref = read.fasta(fasta)
 	genome_size = getLength(ref)
 	wga_reads_tmp = unstack(wga_reads[2:1])
 	nat_reads_tmp = unstack(nat_reads[2:1])
-
-	save(nat_reads_tmp,wga_reads_tmp,genome_size,file = paste("unstacked_ipds.RData",sep = ""))
+	if (save_intermediary_files){
+		save(nat_reads_tmp,wga_reads_tmp,genome_size,file = paste("unstacked_ipds.RData",sep = ""))
+	}
 }
