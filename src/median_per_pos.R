fac091136f02bd423f841619774c617879aaca26
noambeckmann <noambeckmann@gmail.com>
2014-03-10 14:44:51 -0400

    save_intermediary_files

--- src/median_per_pos.R
+++ src/median_per_pos.R
@@ -1,4 +1,4 @@
-median_per_pos = function(pwd){
+median_per_pos = function(pwd,save_intermediary_files){
   #################
   # This function computes the median of the sequenced sample for the coverage wanted
   # median_nat is the matrix with column 1 being positions and column 2 being IPDs
@@ -6,9 +6,12 @@ median_per_pos = function(pwd){
   # genome size is the genome size of the genome of interest
   # pwd is the pathway to the unstacked IPD files of interest
   # Positive strand is in column 1, negative strand is in column 2
+  # save_intermediary_files is a boolean that specifies whether to save intermediary files or not
   ##################
-  load(paste(pwd,"/unstacked_ipds.RData",sep=""))
-
+  if (save_intermediary_files){
+    load(paste(pwd,"/unstacked_ipds.RData",sep=""))
+  }
+  
   median_nat_tmp = matrix(data = NA,nrow = genome_size,ncol = 2)
   index = as.numeric(names(nat_reads_tmp))
   index_pos = abs(index[index<0])
@@ -39,5 +42,7 @@ median_per_pos = function(pwd){
   median_wga = median_wga_tmp
   colnames(median_wga) = c("positive_strand","negative_strand")
 
+  if (save_intermediary_files){
   save(median_nat,median_wga,genome_size,file = paste("median_of_reads_per_position.RData",sep = ""))
+  }
 }
